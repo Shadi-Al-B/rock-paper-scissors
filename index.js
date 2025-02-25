e02893f25da347a53b8816a3c36726db51fdb316
Shadi-Al-B <shadi.albarnouti@gmail.com>
2025-02-25 04:23:23 +0300

    Add UI

--- index.js
+++ index.js
@@ -3,13 +3,18 @@
 let humanScore = 0;
 let computerScore = 0;
 let Draws = 0;
-// Player gives an input
-
-function getHumanChoice() {
-  let humanChoice = prompt("Choose (1) Rock , (2) Paper , (3) Scissors");
-
-  return humanChoice;
+let round = 0;
+function capitalizeFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
+
+// Player gives an input
+//
+// function getHumanChoice() {
+//   let humanChoice = prompt("Choose (1) Rock , (2) Paper , (3) Scissors");
+//
+//   return humanChoice;
+// }
 
 // Computer gives random output
 
@@ -31,45 +36,94 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+const roundResult = document.querySelector(".round-result");
+const displayContainer = document.querySelector(".display-container");
 // Play Round
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = getHumanChoice().toLowerCase();
+  round++;
+  document.querySelector("#round-score").textContent = round;
+  humanChoice = humanChoice.toLowerCase();
   computerChoice = getComputerChoice();
 
   if (humanChoice === computerChoice) {
+    roundResult.textContent = "it is a Draw!";
     console.log("it is a Draw!");
     Draws++;
+    document.querySelector("#draws-score").textContent = Draws;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "rock")
   ) {
+    roundResult.textContent = `You Won! ${capitalizeFirstLetter(
+      humanChoice
+    )} beats ${capitalizeFirstLetter(computerChoice)}`;
+
     console.log("You Won! " + humanChoice + " beats " + computerChoice);
     humanScore++;
+    document.querySelector("#player-score").textContent = humanScore;
   } else {
+    roundResult.textContent = `You Lost! ${capitalizeFirstLetter(
+      humanChoice
+    )} beats ${capitalizeFirstLetter(computerChoice)}`;
+
     console.log("You Lost! " + computerChoice + " beats " + humanChoice);
     computerScore++;
+    document.querySelector("#computer-score").textContent = computerScore;
+  }
+  if (round === 5) {
+    showFinalScore();
+  }
+  if (round === 6) {
+    resetScore();
   }
 }
 
+// Add an event listener to the buttons that call your playRound function with
+// the correct playerSelection every time a button is clicked.
+
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
+
+function clickButton(e) {
+  playRound(e.target.textContent, getComputerChoice());
+}
+
+rockBtn.addEventListener("click", clickButton);
+paperBtn.addEventListener("click", clickButton);
+scissorsBtn.addEventListener("click", clickButton);
+
 function showFinalScore() {
-  console.log("Player Score: " + humanScore);
-  console.log("Computer Score: " + computerScore);
-  console.log("Draws: " + Draws);
+  const gameOver = document.createElement("p");
+  displayContainer.appendChild(gameOver);
+  displayContainer.removeChild(roundResult);
+  gameOver.style.textAlign = "left";
+  gameOver.innerText = "******GAME OVER******\n\n";
+  gameOver.innerText += `Player Score: ${humanScore}\n`;
+  gameOver.innerText += `Computer Score: ${computerScore}\n`;
+  gameOver.innerText += `Draws: ${Draws}\n\n`;
+  gameOver.innerText += "*********************\n\n";
+
   if (humanScore === computerScore) {
-    console.log("Winner: Its a Draw");
+    gameOver.innerText += "Winner: Its a Draw";
   } else if (humanScore > computerScore) {
-    console.log("Player Won!");
+    gameOver.innerText += "Player Won!";
   } else {
-    console.log("Computer Won!");
+    gameOver.innerText += "Computer Won!";
   }
+
+  // console.log("Player Score: " + humanScore);
+  // console.log("Computer Score: " + computerScore);
+  // console.log("Draws: " + Draws);
 }
 
 function resetScore() {
   humanScore = 0;
   computerScore = 0;
   Draws = 0;
+  round = -1;
 }
 
 // Play Game
